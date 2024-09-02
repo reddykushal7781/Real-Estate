2c19ec29e2f44ff894b4cd1df14bd25d00590237
reddykushal777 <reddykushal7781@gmail.com>
2024-09-02 09:15:14 +0530

    final Commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
